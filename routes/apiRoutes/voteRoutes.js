6d10c3e0f65c1dec1b9a035337503da853c05571
Mimi Vo <mimzy414@gmail.com>
2021-06-10 00:49:29 -0500

    vote table

---
+++ routes/apiRoutes/voteRoutes.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+router.post('/vote', ({ body }, res) => {
+    // Data validation
+    const errors = inputCheck(body, 'voter_id', 'candidate_id');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  
+    const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
+    const params = [body.voter_id, body.candidate_id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body,
+        changes: result.affectedRows
+      });
+    });
+  });
+
+
+  router.get('/votes', (req, res) => {
+  const sql = `SELECT * FROM votes WHERE id = ?`;
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+     });
+    });
+  });
+
+
+
+  module.exports = router;
